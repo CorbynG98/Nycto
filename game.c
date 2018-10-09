50e82dbfc1baded160571a0b69f17fa7415aa62b
Benjamin James Harding Scott <bsc47@cs18251kq.canterbury.ac.nz>
2018-10-09 15:33:35 +1300

    added some comments, other minor changes

--- game.c
+++ game.c
@@ -2,24 +2,26 @@
 #include "navswitch.h"
 #include "pacer.h"
 #include <stdbool.h>
+
 #include "disp.h"
+#include "nav.h"
 
 #define PACER_RATE 500
 
 void game_init(int[2] position, char direction) {
     //setup position
-
+    position[0] = 0;
+    position[1] = 0;
+    direction = 'N';
 }
 
 int main (void)
 {
-    int position[2] = {0,0};
-    int last_pos[2] = {0,0};
+    int position[2];
     bool got_minput = false;
-    char direction = 'N';
-    int health = 3;
+    char direction;
 
-    int enemy_position[2] = {0,0};
+    //int health = 3;
 
     //all the usual inits
     system_init ();
@@ -44,7 +46,7 @@ int main (void)
         if (got_minput) {
             if (nav_hit_wall(position, &direction)) {//if hit wall
                 transmit_pos(position);
-                display_self(position);
+                disp_add_self(position);
             } else {
                 nav_move(position, &direction);
             }
@@ -56,22 +58,22 @@ int main (void)
         //receive data and display
         if (rec_got_data()) {
             // if any data received
+
+            //store that data
             char data = rec_get_data();
-            if (rec_is_player(data)) {
-                int[2] player;
-                rec_get_player(player, data);
-                disp_add_player(player);
+
+            //identify data (check if data < 35)
+            if (rec_is_enemy(data)) {
+                int[2] enemy;
+                rec_get_player(enemy, data);//convert data to two integers
+                disp_add_enemy(enemy);
             } else {
                 int[3] laser;
-                rec_get_laser(laser, data);
+                rec_get_laser(laser, data);//convert data to three integers
                 disp_add_laser(laser);
             }
         }
         //do this at required frequency
         disp_update();
-
-        //update vars
-        last_pos[0] = position[0];
-        last_pos[1] = position[1];
     }
 }
